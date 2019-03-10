61648fd42b30bae8063bf9f465a3bf19206be4dc
Jonathan Tuzman <tuzmusic@gmail.com>
2019-03-10 17:02:56 -0400

    implement CoordButton

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,22 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from "react";
+import { debug } from "util";
+
+export default class CoordinatesButton extends Component {
+  constructor(props) {
+    super(props);
+    this.onReceiveCoordinates = props.onReceiveCoordinates;
+    // this.state = { :  }
+  }
+
+  render() {
+    return (
+      <button
+        onClick={event =>
+          this.onReceiveCoordinates([event.screenX, event.screenY])
+        }
+      >
+        BUTTON
+      </button>
+    );
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,11 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = { :  } 
+  };
+  render() {
+    return ""
+  }
+}
