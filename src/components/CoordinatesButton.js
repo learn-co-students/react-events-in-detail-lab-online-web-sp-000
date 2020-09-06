562d5de2bd67834e5d8ff6388aa1f326dde0388b
ali-mohammed200 <ali.mohammed200@gmail.com>
2020-09-06 13:18:24 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,9 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+const CoordinatesButton = (props) => {
+    console.log(props)
+    return <button onClick={(e) => props.onReceiveCoordinates([e.clientX, e.clientY])}>B1</button>
+}
+
+export default CoordinatesButton;
--- test/root.js
+++ test/root.js
@@ -2,7 +2,7 @@ require('babel-register')();
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-var jsdom = require('jsdom').jsdom;
+import { jsdom } from 'jsdom';
 
 global.document = jsdom('');
 global.window = document.defaultView;
