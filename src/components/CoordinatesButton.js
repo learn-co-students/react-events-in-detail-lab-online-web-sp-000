2aec4f9da3d7fc0ef6b0c0fdd0f2dc5ce2ed9ab2
Andrew-J-Williams <1williamsaj@gmail.com>
2020-09-13 12:01:11 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+   
+    constructor(props) { // Create our constructor that accepts the props.
+        super(props);
+    }
+
+    grabCoordinates = (event) => { // We create a function that accepts an event as the argument.
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]) // We grab the X and Y coordinates from the event and pass it into our onReceiveCoordinates prop.
+    }
+
+    render () {
+
+        return (
+            <button onClick={this.grabCoordinates}></button> // Finally, we call this function after a user clicks the button.
+        )
+    }
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,27 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    constructor(props) { // Create our constructor that accepts the props.
+        super(props);
+    }
+
+    withoutDelay = (event) => {
+        event.persist() // We make the event persistent
+
+        const delay = this.props.delay // We define variables for our props, just to make things easier.
+        const onDelayedClick = this.props.onDelayedClick(event)
+
+        setTimeout(
+            () => onDelayedClick, delay // Insert our props into setTimeout, creating a function for our first argument.
+        )
+        
+    }
+
+    render () {
+        return (
+            <button onClick={this.withoutDelay}></button> // On user click, we will run withoutDelay.
+        )
+    }
+
+}
