9ba3092d9c64125497d358c0449f95f2ea13c289
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-09-30 16:51:51 +0100

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,25 @@
-// Code CoordinatesButton Component Here
+// In the components/CoordinatesButton.js file, create a CoordinatesButton React component.
+
+// This component takes in one prop: onReceiveCoordinates. This prop is a function passed down from index.js. This function is currently just logging whatever is passed into it.
+
+// Within CoordinatesButton, render a button. On click of the button, create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
+
+// Pass this event data in as the argument for the onReceiveCoordinates prop.
+
+// If successful, the current x,y position of your mouse should be logged.
+
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  handleClick = event => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>Coords</button>;
+  }
+  
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,24 @@
-// Code DelayedButton Component Here
+// In the components/DelayedButton.js file, create a DelayedButton React component
+
+// This component takes two props: onDelayedClick (a function), and delay (a number).
+
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+
+// If successful, the event will be logged to the console once the timeout has finished.
+
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  handleClick = event => {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>Delayed</button>;
+  }
+}
+
+export default DelayedButton;
