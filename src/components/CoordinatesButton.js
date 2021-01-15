e3aa07237873d54f47daceaf018cfda45be0367c
Yumacorn <ReachRyanMui@gmail.com>
2021-01-15 13:51:37 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,35 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    // callback = (event) => {
+        // let coords = []
+        // console.log(`${props.clientX} ${props.clientY}`)
+        // coords.push([props.clientX, props.clientY])
+    callback = (event) => {
+        // return 7
+        let coords = []
+        // coords.push([event.clientX, event.clientY])
+        coords.push(event.clientX)
+        coords.push(event.clientY)
+
+
+        return this.props.onReceiveCoordinates(coords)
+    }
+        // return (
+        //     this.onReceiveCoordinates([[event.clientX], [event.clientY]])
+        // )
+    // }
+
+    
+    render () {
+        return (
+        <button onClick={this.callback}>Click!</button>
+        )
+    }
+}
